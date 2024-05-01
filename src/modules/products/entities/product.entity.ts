7baf60dec7691c16efd0f0286bc21e645ca64a8e
Ilyosbek <ilyosxonisaqov007@gmail.com>
2024-05-01 23:54:12 +0500

    updated

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -24,7 +24,7 @@ export class ProductEntity {
     @Column({ name: "description", type: "text", nullable: true })
     description: string;
 
-    @Column({ name: "urls", type: "text", nullable: false })
+    @Column('text', { name: "urls", array: true, nullable: false })
     urls: Array<string>;
 
     @Column({ name: "info", type: "json", nullable: false })
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -4,29 +4,29 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany, CreateDateColumn, Up
 @Entity("users")
 export class UserEntity {
     @PrimaryGeneratedColumn()
-     id: number;
+    id: number;
 
     @Column({ name: "first_name", type: "varchar", nullable: false })
     FirstName: string;
 
     @Column({ name: "last_name", type: "varchar", nullable: false })
     LastName: string;
-    
-    @Column({ name: "phones", type: "text"})
+
+    @Column('text', { name: "phones", array: true, nullable: false })
     phones: Array<string>;
     
     @Column({ name: "role", type: "enum", enum: ['admin', 'user', 'owner'], nullable: false })
     role: RoleEnum;
-    
+
     @Column({ name: "username", type: "varchar", unique: true, nullable: false })
     UserName: string;
-    
+
     @Column({ name: "password", type: "varchar", nullable: false })
     password: string;
-    
+
     @Column({ name: "is_active", type: "boolean", nullable: false })
     isActive: boolean;
-    
+
     @CreateDateColumn({
         name: 'created_at',
         type: 'timestamp',
